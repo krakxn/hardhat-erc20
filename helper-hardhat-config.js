a12d17111d3dba22b83e2d9847f779c9d8e2694a
AN <108299225+krakxn@users.noreply.github.com>
2022-07-22 23:10:37 +0530

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,16 +1,16 @@
+// Pricefeed addresses can be obtained at https://docs.chain.link/docs/reference-contracts
+
 const networkConfig = {
     31337: {
       name: "localhost",
     },
-    // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
-    // Default one is ETH/USD contract on Kovan
     42: {
       name: "kovan",
-      ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+      ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331", // Default one is ETH/USD contract on Kovan
     },
   }
+
   const INITIAL_SUPPLY = "1000000000000000000000000"
-  
   const developmentChains = ["hardhat", "localhost"]
   
   module.exports = {
